51e71f3f16333cb88d097cf59918ca2f522244be
William Grant <willmgrant@gmail.com>
2021-06-22 14:08:03 +0200

    updated Hero

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,26 +1,18 @@
 const Hero = () => {
   return (
     <section id='hero'>
-      <div className="profile">
-        <img src='/resources/images/avatar.jpg' alt="profile" className="image" width='175px' height='175px' />
-        <h1 className="heading">William Grant</h1>
+      <div className='intro'>
+        <h1>Hello there <span role='img' alt='waving hand'>👋</span></h1>
+        <h2>My name is <span className='special'>William Grant</span> ,</h2>
+        <h2>the <span className='special'>Ramen Enthusiast <span className="icon" role='img' aria-label='ramen bowl'>🍜</span></span> ,</h2>
+        <h2><span className='special'>Web Developer <span className="icon" role='img' aria-label='programmer'>👨‍💻</span></span> ,</h2>
+        <h2><span className='special'>South African <span className="icon" role='img' aria-label='south africa'>🇿🇦</span></span> ,</h2>
+        <h2><span className='special'>Music Lover <span className="icon" role='img' aria-label='headphones'>🎧</span></span> ,</h2>
+        <h3>... in no particular order .</h3>
       </div>
-    <p className="quote"><i>"Without music, life would be a mistake." - Friedrich Nietzsche</i></p>
-
-    <table className="info-grid">
-      <tbody>
-        <tr>
-          <td className="subtitle left">South African <span className="icon" role='img' aria-label='south africa'>🇿🇦</span></td>
-          <td className="subtitle right">Web Developer <span className="icon" role='img' aria-label='programmer'>👨‍💻</span></td>
-        </tr>
-        <tr>
-          <td className="subtitle left">Music Lover <span className="icon" role='img' aria-label='headphones'>🎧</span></td>
-          <td className="subtitle right">Ramen Enthusiast <span className="icon" role='img' aria-label='ramen bowl'>🍜</span></td>
-        </tr>
-      </tbody>
-    </table>
-    <svg className='scroller left' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
-    <svg className='scroller right' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
+      <div className='scroller'>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" /></svg>
+      </div>
   </section>
   );
 };
